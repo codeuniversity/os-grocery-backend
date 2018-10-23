aff938ba8e2b661ca9c96ba812c2376808d32be0
Jannis Jorre <jeyj0.plus@gmail.com>
2018-10-23 15:04:29 +0200

    Add necessary annotations and parent classes to newly created classes

--- src/main/kotlin/code/osgroceries/domain/supermarket/SupermarketMutation.kt
+++ src/main/kotlin/code/osgroceries/domain/supermarket/SupermarketMutation.kt
@@ -1,9 +1,12 @@
 package code.osgroceries.domain.supermarket
 
 import code.osgroceries.util.generateId
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class SupermarketMutation @Autowired constructor(val supermarketRepository: SupermarketRepository) {
+@Service
+class SupermarketMutation @Autowired constructor(val supermarketRepository: SupermarketRepository) : GraphQLMutationResolver {
 
     fun createSupermarket(name: String) =
             supermarketRepository.insert(Supermarket(generateId(), name))
--- src/main/kotlin/code/osgroceries/domain/supermarket/SupermarketQuery.kt
+++ src/main/kotlin/code/osgroceries/domain/supermarket/SupermarketQuery.kt
@@ -1,8 +1,11 @@
 package code.osgroceries.domain.supermarket
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class SupermarketQuery @Autowired constructor(val supermarketRepository: SupermarketRepository) {
+@Service
+class SupermarketQuery @Autowired constructor(val supermarketRepository: SupermarketRepository) : GraphQLQueryResolver {
 
     fun getAllSupermarkets() = supermarketRepository.findAll()
 }
