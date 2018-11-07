9a871481a154e53a1df9fe822671e240cb456fb8
Jannis Jorre <jeyj0.plus@gmail.com>
2018-11-07 19:15:39 +0100

    ListItemResolver

--- src/main/kotlin/code/osgroceries/domain/listitem/ListItemResolver.kt
+++ src/main/kotlin/code/osgroceries/domain/listitem/ListItemResolver.kt
@@ -1,11 +1,15 @@
 package code.osgroceries.domain.listitem
 
 import code.osgroceries.domain.item.Item
+import code.osgroceries.domain.item.services.GetItemService
 import com.coxautodev.graphql.tools.GraphQLResolver
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class ListItemResolver : GraphQLResolver<ListItem> {
+class ListItemResolver @Autowired constructor(
+        private val getItemService: GetItemService
+) : GraphQLResolver<ListItem> {
 
-    fun item(listItem: ListItem): Item = TODO()
+    fun item(listItem: ListItem): Item = getItemService.getItemById(listItem.itemId)
 }
