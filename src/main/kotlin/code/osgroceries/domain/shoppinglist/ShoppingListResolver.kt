47deb1eac12bd681eb01fb74805856520938bc04
Jannis Jorre <jeyj0.plus@gmail.com>
2018-11-07 19:14:35 +0100

    ShoppingListResolver

--- src/main/kotlin/code/osgroceries/domain/shoppinglist/ShoppingListResolver.kt
+++ src/main/kotlin/code/osgroceries/domain/shoppinglist/ShoppingListResolver.kt
@@ -1,11 +1,15 @@
 package code.osgroceries.domain.shoppinglist
 
 import code.osgroceries.domain.item.Item
+import code.osgroceries.domain.item.services.GetItemService
 import com.coxautodev.graphql.tools.GraphQLResolver
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class ShoppingListResolver : GraphQLResolver<ShoppingList> {
+class ShoppingListResolver @Autowired constructor(
+        private val getItemService: GetItemService
+) : GraphQLResolver<ShoppingList> {
 
-    fun items(shoppingList: ShoppingList): List<Item> = TODO()
+    fun items(shoppingList: ShoppingList): List<Item> = shoppingList.listItems.map { getItemService.getItemById(it.itemId) }
 }
