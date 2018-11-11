a1316122e0a017f56b34d117fd7e1675eb9a7433
Jannis Jorre <jeyj0.plus@gmail.com>
2018-11-11 12:58:25 +0100

    SupermarketResolver

--- src/main/kotlin/code/osgroceries/domain/supermarket/SupermarketResolver.kt
+++ src/main/kotlin/code/osgroceries/domain/supermarket/SupermarketResolver.kt
@@ -1,11 +1,16 @@
 package code.osgroceries.domain.supermarket
 
 import code.osgroceries.domain.row.Row
+import code.osgroceries.domain.row.services.GetRowService
+import code.osgroceries.domain.row.services.GetRowsService
 import com.coxautodev.graphql.tools.GraphQLResolver
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class SupermarketResolver : GraphQLResolver<Supermarket> {
+class SupermarketResolver @Autowired constructor(
+        private val getRowsService: GetRowsService
+) : GraphQLResolver<Supermarket> {
 
-    fun rows(supermarket: Supermarket): List<Row> = TODO()
+    fun rows(supermarket: Supermarket): List<Row> = getRowsService.getRowsBySupermarketId(supermarket.id)
 }
